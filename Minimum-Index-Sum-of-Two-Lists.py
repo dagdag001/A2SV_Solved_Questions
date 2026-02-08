2cfc8ec0772e575a0efae7ca2a667b6f0c126f6f
Dagim Wubeante <dagimw14@gmail.com>
2026-02-08 15:17:14 +0300

    solved

--- Minimum-Index-Sum-of-Two-Lists.py
+++ Minimum-Index-Sum-of-Two-Lists.py
@@ -1,21 +1,18 @@
-1from typing import List
-2
-3class Solution:
-4    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-5        lookUp = {val: idx for idx, val in enumerate(list2)}
-6        min_sum = float("inf")
-7        result = []
-8        
-9        for i in range(len(list1)):
-10            if list1[i] in lookUp:
-11                s = i + lookUp[list1[i]]
-12                
-13                if s < min_sum:
-14                    min_sum = s
-15                    result = [list1[i]]
-16                    
-17                elif s == min_sum:
-18                    result.append(list1[i])
-19        
-20        return result
-21
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        lookUp = {val: idx for idx, val in enumerate(list2)}
+        min_sum = float("inf")
+        result = []
+        
+        for i in range(len(list1)):
+            if list1[i] in lookUp:
+                s = i + lookUp[list1[i]]
+                
+                if s < min_sum:
+                    min_sum = s
+                    result = [list1[i]]
+                    
+                elif s == min_sum:
+                    result.append(list1[i])
+        
+        return result
