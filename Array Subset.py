4c99b8b8b92b23fd75a591fa1792810c7be42fb2
Dagim Wubeante <dagimw14@gmail.com>
2026-02-08 15:30:11 +0300

    solved

---
+++ Array Subset.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+class Solution:
+    #Function to check if a is a subset of b.
+    def isSubset(self, a, b):
+        # Your code here
+        return set(b).issubset(set(a))
+    
+    
+    
