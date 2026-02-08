3ac6c0402c837cd6203e95024a031e16d31033f4
Dagim Wubeante <dagimw14@gmail.com>
2026-02-08 16:15:48 +0300

    solved

--- Majority-Element-II.py
+++ Majority-Element-II.py
@@ -1,10 +1,9 @@
-1class Solution:
-2    def majorityElement(self, nums: List[int]) -> List[int]:
-3        n = len(nums)
-4        count = Counter(nums)
-5        res = []
-6        for num, freq in count.items():
-7            if freq > n // 3:
-8                res.append(num)
-9        return res
-10
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        count = Counter(nums)
+        res = []
+        for num, freq in count.items():
+            if freq > n // 3:
+                res.append(num)
+        return res
