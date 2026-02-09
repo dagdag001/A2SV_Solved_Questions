89e2d7dc72a4900234ca79e5c8db78d3003f3dd9
Dagim Wubeante <dagimw14@gmail.com>
2026-02-09 15:53:38 +0300

    solved

--- Separate-the-Digits-in-an-Array.py
+++ Separate-the-Digits-in-an-Array.py
@@ -1,16 +1,12 @@
-1class Solution:
-2    def separateDigits(self, nums: List[int]) -> List[int]:
-3        def separate(num):
-4            res = []
-5            while num > 0:
-6                res.append(num % 10)
-7                num //= 10
-8            return res[::-1]
-9        res = []
-10        for i in nums:
-11            res.extend(separate(i))
-12        return res
-13             
-14
-15
-16
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        def separate(num):
+            res = []
+            while num > 0:
+                res.append(num % 10)
+                num //= 10
+            return res[::-1]
+        res = []
+        for i in nums:
+            res.extend(separate(i))
+        return res
