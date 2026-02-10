83f6e0d904aabede8291c87ae8a0bf5f590cbb4b
Dagim Wubeante <dagimw14@gmail.com>
2026-02-10 09:17:23 +0300

    solved

--- Insert-Delete-GetRandom-O(1).py
+++ Insert-Delete-GetRandom-O(1).py
@@ -1,42 +1,38 @@
-1class RandomizedSet:
-2
-3    def __init__(self):
-4        self.arr = []
-5        self.pos = {}
-6        
-7
-8    def insert(self, val: int) -> bool:
-9        if val in self.pos:
-10            return False
-11        self.arr.append(val)
-12        self.pos[val] = len(self.arr) - 1 #last index
-13        return True
-14
-15    def remove(self, val: int) -> bool:
-16        if val not in self.pos:
-17            return False
-18        idx = self.pos[val]              
-19        last = self.arr[-1]              
-20
-21        self.arr[idx] = last
-22        self.pos[last] = idx
-23
-24        self.arr.pop()
-25        del self.pos[val]
-26
-27        return True
-28        
-29
-30    def getRandom(self) -> int:
-31        idx = random.randint(0, len(self.arr) - 1)
-32        return self.arr[idx]
-33
-34        
-35        
-36
-37
-38# Your RandomizedSet object will be instantiated and called as such:
-39# obj = RandomizedSet()
-40# param_1 = obj.insert(val)
-41# param_2 = obj.remove(val)
-42# param_3 = obj.getRandom()
+class RandomizedSet:
+
+    def __init__(self):
+        self.arr = []
+        self.pos = {}
+        
+
+    def insert(self, val: int) -> bool:
+        if val in self.pos:
+            return False
+        self.arr.append(val)
+        self.pos[val] = len(self.arr) - 1 #last index
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.pos:
+            return False
+        idx = self.pos[val]              
+        last = self.arr[-1]              
+
+        self.arr[idx] = last
+        self.pos[last] = idx
+
+        self.arr.pop()
+        del self.pos[val]
+
+        return True
+        
+
+    def getRandom(self) -> int:
+        idx = random.randint(0, len(self.arr) - 1)
+        return self.arr[idx]
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
