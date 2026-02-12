71f2cdfd0b9461bbcd90f5a765f96b630934bfac
Dagim Wubeante <dagimw14@gmail.com>
2026-02-12 11:36:29 +0300

    solved

--- Find-Original-Array-From-Doubled-Array.py
+++ Find-Original-Array-From-Doubled-Array.py
@@ -1,20 +1,18 @@
-1class Solution:
-2    def findOriginalArray(self, changed: List[int]) -> List[int]:
-3        if len(changed) % 2 != 0:
-4            return []
-5        res = []
-6        cc = Counter(changed)
-7        for i in sorted(cc):
-8            if i == 0:
-9                if cc[0] % 2 != 0:
-10                    return []
-11                res.extend([0] * (cc[0] // 2))
-12                continue
-13            if cc[i] > cc[i*2]:
-14                return []
-15            for _ in range(cc[i]):
-16                res.append(i)
-17                cc[i*2] -=1
-18        return res
-19
-20        
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        if len(changed) % 2 != 0:
+            return []
+        res = []
+        cc = Counter(changed)
+        for i in sorted(cc):
+            if i == 0:
+                if cc[0] % 2 != 0:
+                    return []
+                res.extend([0] * (cc[0] // 2))
+                continue
+            if cc[i] > cc[i*2]:
+                return []
+            for _ in range(cc[i]):
+                res.append(i)
+                cc[i*2] -=1
+        return res
--- Minimum-Number-of-Steps-to-Make-Two-Strings-Anagram.py
+++ Minimum-Number-of-Steps-to-Make-Two-Strings-Anagram.py
@@ -1,7 +1,7 @@
-1class Solution:
-2    def minSteps(self, s: str, t: str) -> int:
-3        ct = Counter(t)
-4        cs = Counter(s)
-5        union = cs | ct
-6        intersection = union & cs
-7        return sum(union.values()) - sum(intersection.values()) 
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        ct = Counter(t)
+        cs = Counter(s)
+        union = cs | ct
+        intersection = union & cs
+        return sum(union.values()) - sum(intersection.values()) 
