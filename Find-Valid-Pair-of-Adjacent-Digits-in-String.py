5482472ee669a7fcc740ce73869237ea7e2ca690
Dagim Wubeante <dagimw14@gmail.com>
2026-02-12 10:36:57 +0300

    solved

--- Find-Valid-Pair-of-Adjacent-Digits-in-String.py
+++ Find-Valid-Pair-of-Adjacent-Digits-in-String.py
@@ -1,13 +1,12 @@
-1class Solution:
-2    def findValidPair(self, s: str) -> str:        
-3        freq = Counter(s)      
-4        n = len(s)
-5        for i in range(n - 1):
-6            a = s[i]
-7            b = s[i + 1]
-8            if a != b:
-9                if freq[a] == int(a) and freq[b] == int(b):
-10                    return a + b
-11        
-12        return ""
-13
+class Solution:
+    def findValidPair(self, s: str) -> str:        
+        freq = Counter(s)      
+        n = len(s)
+        for i in range(n - 1):
+            a = s[i]
+            b = s[i + 1]
+            if a != b:
+                if freq[a] == int(a) and freq[b] == int(b):
+                    return a + b
+        
+        return ""
