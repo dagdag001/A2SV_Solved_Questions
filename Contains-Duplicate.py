4dd78ac4e51107dd009ef1b14399193caf36b4f1
Dagim Wubeante <dagimw14@gmail.com>
2026-02-12 10:28:26 +0300

    [217] [Time Beats: 66.95%] [Memory Beats: 56.71%] - LeetPush

---
+++ Contains-Duplicate.py
@@ -0,0 +1,9 @@
+1class Solution:
+2    def containsDuplicate(self, nums: List[int]) -> bool:
+3        seen = set()
+4        for num in nums:
+5            if num in seen:
+6                return True
+7            seen.add(num)
+8            
+9        return False
