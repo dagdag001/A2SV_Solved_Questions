19edebbe4fe6398ab7fcfc9026e63116debca5ff
Dagim Wubeante <dagimw14@gmail.com>
2026-02-13 11:35:49 +0300

    solved

--- Count-Equal-and-Divisible-Pairs-in-an-Array.py
+++ Count-Equal-and-Divisible-Pairs-in-an-Array.py
@@ -1,9 +1,8 @@
-1class Solution:
-2    def countPairs(self, nums: List[int], k: int) -> int:
-3        count = 0
-4        for i in range(len(nums)-1):
-5            for j in range(i+1 ,len(nums)):
-6                if nums[i] == nums[j] and (i*j) % k == 0:
-7                    count+=1
-8        return count
-9
+class Solution:
+	def countPairs(self, nums: List[int], k: int) -> int:
+		count = 0
+		for i in range(len(nums) - 1):
+			for j in range(i + 1, len(nums)):
+				if nums[i] == nums[j] and (i * j) % k == 0:
+					count += 1
+		return count
--- Word-Pattern.py
+++ Word-Pattern.py
@@ -1,24 +1,23 @@
-1class Solution:
-2    def wordPattern(self, pattern: str, s: str) -> bool:
-3        list_p = list(pattern)
-4        list_s = s.split()
-5        if len(list_s ) != len(list_p):
-6            return False
-7        res = list(zip(list_s, list_p))
-8        h = {}
-9        for i ,j in res:
-10            if i not in h:
-11                h[i] = [j]
-12            else:
-13                h[i].append(j)
-14        for p, s  in h.items():
-15            if len(set(s)) != 1:
-16                return False
-17        used = set()
-18        for lst in h.values():
-19            word = lst[0]
-20            if word in used:
-21                return False
-22            used.add(word)
-23        return True
-24
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        list_p = list(pattern)
+        list_s = s.split()
+        if len(list_s) != len(list_p):
+            return False
+        res = list(zip(list_s, list_p))
+        h = {}
+        for i, j in res:
+            if i not in h:
+                h[i] = [j]
+            else:
+                h[i].append(j)
+        for p, s in h.items():
+            if len(set(s)) != 1:
+                return False
+        used = set()
+        for lst in h.values():
+            word = lst[0]
+            if word in used:
+                return False
+            used.add(word)
+        return True
