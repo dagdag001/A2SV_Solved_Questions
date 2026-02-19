d93a7b3d464b38b2153e5ada10c41f58de048f4f
Dagim Wubeante <dagimw14@gmail.com>
2026-02-19 18:47:42 +0300

    solved

--- Count-Negative-Numbers-in-a-Sorted-Matrix.py
+++ Count-Negative-Numbers-in-a-Sorted-Matrix.py
@@ -1,8 +1,8 @@
-1class Solution:
-2    def countNegatives(self, grid: List[List[int]]) -> int:
-3        neg =0
-4        for row in grid:
-5            for val in row:
-6                if val < 0:
-7                    neg +=1
-8        return neg
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        neg =0
+        for row in grid:
+            for val in row:
+                if val < 0:
+                    neg +=1
+        return neg
--- Rotate-Image.py
+++ Rotate-Image.py
@@ -1,13 +1,12 @@
-1class Solution:
-2    def rotate(self, matrix: List[List[int]]) -> None:
-3        """
-4        Do not return anything, modify matrix in-place instead.
-5        """
-6        # TRANSPOSE then REVERSE THE ROW
-7        row = len(matrix)
-8        for i in range(row):
-9            for j in range(i+1, row):
-10                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-11        for i in (matrix):
-12            i.reverse()
-13
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # TRANSPOSE then REVERSE THE ROW
+        row = len(matrix)
+        for i in range(row):
+            for j in range(i+1, row):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in (matrix):
+            i.reverse()
