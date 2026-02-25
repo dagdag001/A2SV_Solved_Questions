ab2e54bc044c10a571dfffb2a6e79a6041610558
Dagim Wubeante <dagimw14@gmail.com>
2026-02-25 17:47:50 +0300

    [28] [Time Beats: 100.00%] [Memory Beats: 50.61%] - LeetPush

--- Find-the-Index-of-the-First-Occurrence-in-a-String.py
+++ Find-the-Index-of-the-First-Occurrence-in-a-String.py
@@ -1,6 +1,6 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(len(haystack) - len(needle) + 1):
-            if haystack[i:i + len(needle)] == needle:
-                return i
-        return -1
+1class Solution:
+2    def strStr(self, haystack: str, needle: str) -> int:
+3        for i in range(len(haystack) - len(needle) + 1):
+4            if haystack[i:i + len(needle)] == needle:
+5                return i
+6        return -1
