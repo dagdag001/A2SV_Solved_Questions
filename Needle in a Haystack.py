fcd2c71bbae3b65386cceeb917bc2d1ee76b366f
Dagim Wubeante <dagimw14@gmail.com>
2026-02-27 18:01:16 +0300

    solved

--- Needle in a Haystack.py
+++ Needle in a Haystack.py
@@ -1,20 +1,22 @@
 from collections import Counter
-t = int(input())
-for _ in range(t):
-    s = input()
-    t = input()
-    cs = Counter(s)
-    ct = Counter(t)
-    for ch in cs:
-        if ct[ch] < cs[ch]:
-            print("IMPOSSIBLE")
-            continue
-        
-    count = ct- cs
-    res = []
-    for char in count:
-        for _ in range(count[char]):
-            res.append(char)
-    res.append(s)
-    res.sort()
-    print("".join(res))
+
+T = int(input())
+for _ in range(T):
+    s = input().strip()
+    t_str = input().strip()
+    
+    if Counter(s) - Counter(t_str):
+        print("Impossible")
+        continue
+    
+    extra = sorted((Counter(t_str) - Counter(s)).elements())
+    
+    c = s[0]
+    less = [x for x in extra if x < c]
+    equal = [x for x in extra if x == c]
+    greater = [x for x in extra if x > c]
+    
+    option1 = "".join(less) + s + "".join(equal) + "".join(greater)
+    option2 = "".join(less) + "".join(equal) + s + "".join(greater)
+    
+    print(min(option1, option2))
