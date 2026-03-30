3e4c073242230bdb1d66320be9ee76a6fa72cce4
Dagim Wubeante <dagimw14@gmail.com>
2026-03-30 17:43:07 +0300

    Add solution for Coloring Game

---
+++ codeforces/2112C-Coloring-Game/2112C-Coloring-Game.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def solve():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        
+        ans = 0
+        M = a[-1]
+        
+        for k in range(2, n):
+            need = max(a[k], M - a[k])
+            
+            i, j = 0, k - 1
+            while i < j:
+                if a[i] + a[j] > need:
+                    ans += (j - i)
+                    j -= 1
+                else:
+                    i += 1
+        
+        print(ans)
+
+solve()
