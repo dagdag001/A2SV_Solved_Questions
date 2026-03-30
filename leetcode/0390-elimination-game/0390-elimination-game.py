8c36c049fff0d6c8e618f8be74a84fa330f2fe20
Dagim Wubeante <dagimw14@gmail.com>
2026-03-30 17:53:55 +0300

    Add solution for Elimination Game

---
+++ leetcode/0390-elimination-game/0390-elimination-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lastRemaining(self, n: int) -> int:
+        head = 1
+        step = 1
+        left_to_right = True
+        remaining = n
+        
+        while remaining > 1:
+            if left_to_right or remaining % 2 == 1:
+                head += step
+            step *= 2
+            remaining //= 2
+            left_to_right = not left_to_right
+        
+        return head
