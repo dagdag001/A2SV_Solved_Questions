5af2e76979ba1c71b9d289c4f5a7ea8ebc05352b
Dagim Wubeante <dagimw14@gmail.com>
2026-04-13 21:10:05 +0300

    Add solution for Magnetic Force Between Two Balls

---
+++ leetcode/1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxDistance(self, position, m):
+        position.sort()
+
+        def can_place(d):
+            count = 1
+            last = position[0]
+
+            for i in range(1, len(position)):
+                if position[i] - last >= d:
+                    count += 1
+                    last = position[i]
+                    if count >= m:
+                        return True
+            return False
+
+        left, right = 1, position[-1] - position[0]
+
+        while left <= right:
+            mid = (left + right) // 2
+            if can_place(mid):
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return right
+        
